e91d47786905a059fb282b5e219204c529dad31a
HexMagnus <luke.dev.fullstack@outlook.com>
2021-11-22 18:17:17 -0300

    Update
    update

--- Aula 11/exe008.js
+++
@@ -1,6 +0,0 @@
-var vel = 12
-console.log(`a velocidade do seu carro ${vel}km/h`)
-if (vel > 60) {
-    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
-}
-console.log(`Dirija sempre usando o cinto de segurança`)
--- Aula 11/exe009.js
+++
@@ -1,7 +0,0 @@
-var país = 'EUA'
-console.log(`vivendo em ${país}`)
-if (país == 'Brasil') {
-    console.log('Você éBrasileiro!')
-} else {
-    console.log('Você é Estrangeiro!')
-}
